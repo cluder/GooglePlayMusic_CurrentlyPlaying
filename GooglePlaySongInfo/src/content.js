0afbd1fe97e91b845ccd0daf64577d9b4020290e
cluder <cluder@noreply.users.github.com>
2020-07-01 23:11:29 +0200

    added youtube music support
    - v2.0

--- GooglePlaySongInfo/src/content.js
+++ GooglePlaySongInfo/src/content.js
@@ -4,42 +4,60 @@
 setInterval(function() {
 	console.log('checking song info ...');
 	var title = '';
-	var album = '';
 	var artist = '';
 	var imgUrl = '';
 	var isPlaying = 'false';
+    
+    var cur_playing_title;
+    var player_artist;
+    var playerBarArt;
+    var play_pause_button;
 
-	let cur_playing_title = document.getElementById('currently-playing-title');
+    function isYouTube() {return window.location.toString().includes("music.youtube");}
+    function isPlayMusic() {return url.includes("play.google");}
+
+	if (isYouTube()) {
+		var complexString = document.getElementsByClassName("ytmusic-player-bar complex-string")[0];
+		
+        cur_playing_title = document.getElementsByClassName('yt-simple-endpoint style-scope yt-formatted-string')[1];
+        player_artist = document.getElementsByClassName('yt-simple-endpoint style-scope yt-formatted-string')[0];
+		playerBarArt = document.getElementsByClassName('image style-scope ytmusic-player-bar')[0];
+		play_pause_button = document.getElementById('play-pause-button');
+		if (play_pause_button != null) {
+			if (play_pause_button.title.toLowerCase().includes('paus')) {
+				isPlaying = 'true';
+			} else {
+				isPlaying = 'false';
+			}
+		}
+	} else if (isPlayMusic()) {
+		cur_playing_title = document.getElementById('currently-playing-title');
+		player_artist = document.getElementById('player-artist');
+		playerBarArt = document.getElementById('playerBarArt');
+		play_pause_button = document.getElementById('player-bar-play-pause');
+
+		if (play_pause_button != null) {
+			if (play_pause_button.className.includes('playing')) {
+				isPlaying = 'true';
+			} else {
+				isPlaying = 'false';
+			}
+		}
+	}
+
 	if (cur_playing_title != null) {
 		title = cur_playing_title.innerText;
 	}
 
-	let player_artist = document.getElementById('player-artist');
 	if (player_artist != null) {
 		artist = player_artist.innerText;
 	}
 
-	let player_albums = document.getElementsByClassName('player-album');
-	if (player_albums.length > 0) {
-		album = player_albums[0].innerText;
-	}
-
-	// player art
-	let playerBarArt = document.getElementById('playerBarArt');
 	if (playerBarArt != null) {
 		imgUrl = playerBarArt.src;
 	}
 
-	let play_pause_button = document.getElementById('player-bar-play-pause');
-	if (play_pause_button != null) {
-		if (play_pause_button.className.includes('playing')) {
-			isPlaying = 'true';
-		} else {
-			isPlaying = 'false';
-		}
-	}
-
-	var params = 'title=' + title + '&album=' + album + '&artist=' + artist
+	var params = 'title=' + title + '&artist=' + artist
 			+ '&imgUrl=' + imgUrl + '&isPlaying=' + isPlaying;
 
 	var xhr = new XMLHttpRequest();
